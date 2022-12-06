1076cf686531b8f4c89a763e61039bca0b2271d9
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2022-12-06 08:53:07 -0800

    Add items for three groups.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
 use yew::prelude::*;
 
+const BASE_URL: &'static str = "https://topaz-utopian-khaan.glitch.me";
+
+struct StudyGroup {
+	start: usize,
+	end: usize,
+}
+
+impl StudyGroup {
+	pub fn url(&self) -> String {
+		format!("{}?start={}&end={}", BASE_URL, self.start, self.end)
+	}
+	pub fn label(&self) -> String {
+		format!("Chapters {}-{}", self.start, self.end)
+	}
+}
+
 #[function_component(App)]
 fn app() -> Html {
+	let groups = vec![
+		StudyGroup { start: 1, end: 5 },
+		StudyGroup { start: 5, end: 10 },
+		StudyGroup { start: 11, end: 15 },
+	];
+	let groups_html = groups.into_iter().map(|group| {
+		let (url, label) = (group.url(), group.label());
+		html! {<li><a href={url}>{label}</a></li>}
+	}).collect::<Html>();
 	html! {
+		<>
 		<h1>{ "Kanjikan Web" }</h1>
+		<ul>{groups_html}</ul>
+		</>
 	}
 }
 
